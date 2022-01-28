5c9a3d96324b6db343998613819fca120491dde9
Scott Larsen <sslarsen411@gmail.com>
2022-01-27 16:24:54 -0800

    First round

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { CardList } from './components/card-list/card-list.component'
+//  const fetch = require('node-fetch')
+class App extends Component {
+  constructor () {
+    super()
+    // this.state = {
+    //   string: 'Hello Scott'
+    // }
+    this.state = {
+      monsters: []
+    }
+  } // END CONSTRUCTOR
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
 
-export default App;
+  render () {
+    return (
+      <div className='App'>
+        <input type='search' placeholder='Search monsters' />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    )
+  } // END RENDER
+} // END CLASS App
+export default App
